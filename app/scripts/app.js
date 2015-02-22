a9bf6bf898373e1f76a680f729562831aa490a3d
manish_office <manish.nasa@gmail.com>
2015-02-22 20:36:01 +0530

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name quizApp
+ * @description
+ * # quizApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('quizApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/question', {
+        templateUrl: 'views/question.html',
+        controller: 'QuestionCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
